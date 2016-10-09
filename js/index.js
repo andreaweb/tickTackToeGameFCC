faed3341c9cb9ae4bc38eb40e99837eba609d6a5
Andrea Santana <Andrea Santana>
2016-10-09 12:13:41 -0300

    making functions out of each step

--- js/index.js
+++ js/index.js
@@ -1,91 +1,99 @@
-//must check if tile already has something
 $(".tile").on("click", function(){
-  if( $(this).hasClass("unavailable") ){
-    return false;
-  }
-  
 
+  var draw = 0;
   var tileOrder = document.querySelectorAll(".tile");
-  var victories = 0;
-  var draws = 0;
-  var defeats = 0;
 
-  $(this).append("X").addClass("unavailable");
-  
+  if( $(this).hasClass("unavailable") ){
+    return false; //first thing we do is check if the click is valid (tile must NOT have something already)
+  }  
+
+  $(this).append("X").addClass("unavailable"); //if click is valid, we can fill the tile with X
+
+  //then we check if there is a victory or draw already
+  checkVictory();
+
+  if(draw > 7){
+    showPopUp(".draws"); 
+  }
+
+  //if not, the machine can make its move
+  addO();
+
+  //then we check if the machine won
+  checkVictory();
+
+  function addO(){
     var selection = [];
-  //check all available (empty) tiles and make a random selection out of them
-  $( ".tile" ).each(function( index ) {
-    
-    if( $(this).hasClass("unavailable")){
+    //check all available (empty) tiles and make a random selection out of them
+    $( ".tile" ).each(function( index ) {
       
-    }else{
-      selection.push(this);
-    }
-    
-  });
-  
-  
-  var machineMove = Math.floor(Math.random() * (selection.length - 0));
-  $(selection[machineMove]).append("O").addClass("unavailable");
-   
-
-    //must check if there is a victory already
-  //victory conditions: all tiles in the same row have the same html, OR
-  //the middle tile of the middle row has the same html as its extreme opposites, OR
-  //the tiles that share the same position in each row (1st, 2nd or 3rd) have the same html
-  
-  //covers third scenario
-  for(var m = 0; m < 3; m++){
-    if($(tileOrder[m]).html() === $(tileOrder[m+3]).html() &&
-       $(tileOrder[m]).html() === $(tileOrder[m+6]).html() && $(tileOrder[m]).hasClass("unavailable") ){
-        if($(tileOrder[m]).html() === "o"){
-          showPopUp(".defeats"); 
-        }else{
-          showPopUp(".victories");
-        }
-    }
-  }
-    
-  
-  // this covers the second scenario
-  if($(tileOrder[4]).html() === $(tileOrder[8]).html() && 
-     $(tileOrder[4]).html() === $(tileOrder[0]).html() && $(tileOrder[0]).hasClass("unavailable") ||
-     $(tileOrder[4]).html() === $(tileOrder[6]).html() &&
-     $(tileOrder[4]).html() === $(tileOrder[2]).html() && $(tileOrder[2]).hasClass("unavailable")){
-        if($(tileOrder[4]).html() === "o"){
-          showPopUp(".defeats"); 
-        }else{
-          showPopUp(".victories");
-        }
-     }
-  
-  
-  var count = 0;
-  
-  //this covers the first scenario
-  var onePerRow = [tileOrder[0], tileOrder[3], tileOrder[6]];
-  for(var i = 0; i < onePerRow.length; i++){
-    var answer = $(onePerRow[i]).html();
-    $(onePerRow[i]).siblings().each( function () {
-      if(answer !== $(this).html() || answer == ""){
-        //alert("not this time");
-        return false;  
+     if( $(this).hasClass("unavailable")){
+        draw++;  
+        console.log(draw);
+      }else{
+        selection.push(this);
       }
-      count++;
-      if(count == 2){
-        if($(this).html() === "o"){
-          showPopUp(".defeats"); 
-        }else{
-          showPopUp(".victories");
-        }
-      }
+      
     });
+    
+    var machineMove = Math.floor(Math.random() * (selection.length - 0));
+    $(selection[machineMove]).append("O").addClass("unavailable");
   }
-  
+
+  function checkVictory(){
+    //covers the tiles that share the same position in each row (1st, 2nd or 3rd) have the same html
+    for(var m = 0; m < 3; m++){
+      if($(tileOrder[m]).html() === $(tileOrder[m+3]).html() &&
+         $(tileOrder[m]).html() === $(tileOrder[m+6]).html() && $(tileOrder[m]).hasClass("unavailable") ){
+          if($(tileOrder[m]).html() === "O"){
+            showPopUp(".defeats"); 
+          }else{
+            showPopUp(".victories");
+          }
+      }
+    }
+      
+    
+    // this covers the middle tile of the middle row has the same html as its extreme opposites
+    if($(tileOrder[4]).html() === $(tileOrder[8]).html() && 
+       $(tileOrder[4]).html() === $(tileOrder[0]).html() && $(tileOrder[0]).hasClass("unavailable") ||
+       $(tileOrder[4]).html() === $(tileOrder[6]).html() &&
+       $(tileOrder[4]).html() === $(tileOrder[2]).html() && $(tileOrder[2]).hasClass("unavailable")){
+          if($(tileOrder[4]).html() === "O"){
+            showPopUp(".defeats"); 
+          }else{
+            showPopUp(".victories");
+          }
+    }
+    
+    
+    var count = 0;
+    
+    //all tiles in the same row have the same html
+    var onePerRow = [tileOrder[0], tileOrder[3], tileOrder[6]];
+    for(var i = 0; i < onePerRow.length; i++){
+      var answer = $(onePerRow[i]).html();
+      $(onePerRow[i]).siblings().each( function () {
+        if(answer !== $(this).html() || answer == ""){
+          //alert("not this time");
+          return false;  
+        }
+        count++;
+        if(count == 2){
+          if($(this).html() === "O"){
+            showPopUp(".defeats"); 
+          }else{
+            showPopUp(".victories");
+          }
+        }
+      });
+    }
+  }
+    
   function showPopUp(result){
-    if(result == "victories"){
+    if(result == ".victories"){
       $(".popup h3").empty().append("You Win!");
-    }else if(result == "defeats"){
+    }else if(result == ".defeats"){
       $(".popup h3").empty().append("You Lose!");
     }else{
       $(".popup h3").empty().append("Draw!");
@@ -100,7 +108,7 @@ $(".tile").on("click", function(){
   });
  
   function victory(){
-    console.log("victory");
+    console.log( $(".popup h3").html());
     for(var j= 0; j < tileOrder.length; j++){
       $(tileOrder[j]).removeClass("unavailable");
       $(tileOrder[j]).empty();
