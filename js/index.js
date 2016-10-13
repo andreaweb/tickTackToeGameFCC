7fd53609139a083974ec39efd9cefe2ed64a33bb
Andrea Santana <deiainluv@gmail.com>
2016-10-13 20:51:55 -0300

    numbers now being added to score - score itself still incorrect

--- js/index.js
+++ js/index.js
@@ -3,22 +3,21 @@ $(".tile").on("click", function(){
   var draw = 0;
   var tileOrder = document.querySelectorAll(".tile");
 
-  if( $(this).hasClass("unavailable") ){
+  if( $(this).hasClass("unavailable") || $(".popup").hasClass("no-more-clicks") ){
     return false; //first thing we do is check if the click is valid (tile must NOT have something already)
   }  
 
   $(this).append("X").addClass("unavailable"); //if click is valid, we can fill the tile with X
 
-  //then we check if there is a victory or draw already
+  //then we check if there is a victory
   checkVictory();
-
-  if(draw > 7){
-    showPopUp(".draws"); 
-  }
 
   //if not, the machine can make its move
   addO();
-
+  
+  //then we check for a draw -> must happen BEFORE checkVictory(), otherwise it overwrites it
+  checkDraw();
+  
   //then we check if the machine won
   checkVictory();
 
@@ -40,16 +39,18 @@ $(".tile").on("click", function(){
     $(selection[machineMove]).append("O").addClass("unavailable");
   }
 
-  function checkVictory(){
+  function checkVictory(){//current issue: need to get out of this function after first showPopUp is called
     //covers the tiles that share the same position in each row (1st, 2nd or 3rd) have the same html
     for(var m = 0; m < 3; m++){
       if($(tileOrder[m]).html() === $(tileOrder[m+3]).html() &&
          $(tileOrder[m]).html() === $(tileOrder[m+6]).html() && $(tileOrder[m]).hasClass("unavailable") ){
-          if($(tileOrder[m]).html() === "O"){
+        console.log("1");  
+        if($(tileOrder[m]).html() === "O"){
             showPopUp(".defeats"); 
           }else{
             showPopUp(".victories");
           }
+        
       }
     }
       
@@ -59,8 +60,10 @@ $(".tile").on("click", function(){
        $(tileOrder[4]).html() === $(tileOrder[0]).html() && $(tileOrder[0]).hasClass("unavailable") ||
        $(tileOrder[4]).html() === $(tileOrder[6]).html() &&
        $(tileOrder[4]).html() === $(tileOrder[2]).html() && $(tileOrder[2]).hasClass("unavailable")){
-          if($(tileOrder[4]).html() === "O"){
+      console.log("2");    
+      if($(tileOrder[4]).html() === "O"){
             showPopUp(".defeats"); 
+           
           }else{
             showPopUp(".victories");
           }
@@ -79,7 +82,8 @@ $(".tile").on("click", function(){
           return false;  
         }
         count++;
-        if(count == 2){
+        if(count > 2){
+          console.log("3");
           if($(this).html() === "O"){
             showPopUp(".defeats"); 
           }else{
@@ -89,8 +93,17 @@ $(".tile").on("click", function(){
       });
     }
   }
+  
+  function checkDraw(){
+    //checks if there's a draw
+    if(draw > 7){
+      showPopUp(".draws"); 
+    }
+  }
     
   function showPopUp(result){
+    var oldValue = $(result).find("p");
+    var updatedValue = parseInt(oldValue.html())+1;
     if(result == ".victories"){
       $(".popup h3").empty().append("You Win!");
     }else if(result == ".defeats"){
@@ -98,12 +111,12 @@ $(".tile").on("click", function(){
     }else{
       $(".popup h3").empty().append("Draw!");
     }
-    $(".popup").show();
-    $(result).find("p").empty().append( parseInt($(result).find("p").html())+1 );
+    $(".popup").show().addClass("no-more-clicks");
+    $(result).find("p").empty().append( updatedValue );
   }
   
   $("button").on("click", function(){
-    $(".popup").hide();
+    $(".popup").hide().removeClass("no-more-clicks");
     victory();
   });
  
