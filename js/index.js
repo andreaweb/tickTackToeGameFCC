8d6d39900ecd2f3576713d9b447d0f318af529c2
Andrea Santana <deiainluv@gmail.com>
2016-10-22 19:38:50 -0200

    improved comments

--- js/index.js
+++ js/index.js
@@ -3,46 +3,45 @@ $(".tile").on("click", function(){
   var draw = 0;
   var tileOrder = document.querySelectorAll(".tile");
 
+  //first thing we do is check if the click is valid (tile must be empty)
   if( $(this).hasClass("unavailable") || $(".popup").hasClass("no-more-clicks") ){
-    return false; //first thing we do is check if the click is valid (tile must be empty)
+    return false; 
   }  
 
-  $(this).append("X").addClass("unavailable"); //if click is valid, we can fill the tile with X
+  //if click is valid, we can fill the tile with X
+  $(this).append("X").addClass("unavailable"); 
 
   
-  //then we check for a draw -> must happen BEFORE checkVictory(), otherwise it overwrites it
-
-  
-   //then we check if there is a victory
+  //then we check if there is a victory
   checkVictoryFirstScenario("X");
   
   function addO(){
     var selection = [];
     
     //check all available (empty) tiles and make a random selection out of them
-    $( ".tile" ).each(function( index ) {
-      
+    $(".tile").each(function(index) {
      if( $(this).hasClass("unavailable")){
         draw++;  
         console.log(draw);
       }else{
         selection.push(this);
-      }
-      
+      }      
     });
 
-     //checks if there's a draw
+    //checks if there's a draw
     if(draw > 7){
       showPopUp(".draws"); 
     }
     
+    //machine makes a move in a random tile //-> for the future, I could make the machine more intelligent
     var machineMove = Math.floor(Math.random() * (selection.length - 0));
     $(selection[machineMove]).append("O").addClass("unavailable");
 
+    //checks if machine made a winning move
     checkVictoryFirstScenario("O");
   }
   
-  //covers the tiles that share the same position in each row (1st, 2nd or 3rd) have the same html
+  //covers the scenario where tiles that share the same position in each row (1st, 2nd or 3rd) have the same html
   function checkVictoryFirstScenario(move){
     for(var m = 0; m < 3; m++){
       if($(tileOrder[m]).html() === $(tileOrder[m+3]).html() &&
@@ -54,12 +53,13 @@ $(".tile").on("click", function(){
             showPopUp(".victories");
           } 
       }else if(m==2){
+      //victory scenario conditions not met, check next scenario
         checkVictorySecondScenario(move);
       }
     }
   }
   
-  // this covers the middle tile of the middle row has the same html as its extreme opposites
+  //covers the scenario where middle tile of the middle row has the same html as its extreme opposites
   function checkVictorySecondScenario(move){
     if($(tileOrder[4]).html() === $(tileOrder[8]).html() && 
        $(tileOrder[4]).html() === $(tileOrder[0]).html() && $(tileOrder[0]).hasClass("unavailable") ||
@@ -72,11 +72,13 @@ $(".tile").on("click", function(){
           }else{
             showPopUp(".victories");
           }
-    }else{
+    }else{ 
+    //victory scenario conditions not met, check next scenario
         checkVictoryThirdScenario(move);
     }
   }   
    
+  //covers the scenario where all the siblings (tiles per row) are equal
   function checkVictoryThirdScenario(move){
     var i = 0;
     while(i < 8){
@@ -91,14 +93,13 @@ $(".tile").on("click", function(){
       }
       i = i + 3;
     }
+    //no victory happened after human's turn, so the machine can make its move
     if(move === "X"){
-      addO(); //no victory happened after human's turn, so the machine can make its move
+      addO(); 
     }
-      
-      //the function is still called, if conditions must be changed
   }
     
- //shows Pop up with result and adequately fills the board
+ //shows pop up with result and adequately fills the scoreboard
   function showPopUp(result){
     var oldValue = $(result).find("p");
     var updatedValue = parseInt(oldValue.html())+1;
